80df0ec2c7a289340a7be6c65c9df59c811c870d
Luis Gerardo <gerch.bbx@gmail.com>
2024-05-06 12:51:53 -0600

    Commit 1 : Se sube el proyecto

---
+++ src/main/java/lggd/uv/mx/demo/TablasApplication.java
@@ -0,0 +1,13 @@
+package lggd.uv.mx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lggd/uv/mx/demo/TablasApplicationTests.java
@@ -0,0 +1,13 @@
+package lggd.uv.mx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
